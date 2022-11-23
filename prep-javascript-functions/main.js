6827830cdcf1ca2f128397ed70aefc1ccc8befb1
Joseph Lee <leejaejoseph@gmail.com>
2022-11-23 06:23:36 +0000

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,45 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+addTwoNumbers(2, 2);
+
+function convertHoursToMinutes(hours) {
+  return (60 * hours);
+}
+convertHoursToMinutes(2);
+
+function getGreeting(name) {
+  return 'Hello' + ' ' + name;
+}
+getGreeting('world');
+
+function addAndMultiplyBy5(num1, num2) {
+  return ((num1 + num2) * 5);
+}
+addAndMultiplyBy5(2, 4);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return ((num1 * num2) / 5);
+}
+
+multiplyAndDivideBy5(2, 4);
+
+function subtractTwoNumbers(num1, num2) {
+  return (num1 - num2);
+}
+subtractTwoNumbers(22, 7);
+
+function getCircleCircumference(radius) {
+  return (2 * 3.14159265358979 * radius);
+}
+getCircleCircumference(4);
+
+function getFullName(firstName, lastName) {
+  return (firstName + ' ' + lastName);
+}
+getFullName('Juan', 'Ramirez');
+
+function cube(number) {
+  return (number * number * number);
+}
+cube(3);
